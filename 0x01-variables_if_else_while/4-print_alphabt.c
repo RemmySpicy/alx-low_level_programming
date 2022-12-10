ee3a8100b5847b05ca40489ed32631cfe3f865ef
Remilekun <remmylknolaotan@gmail.com>
2022-12-09 17:01:07 -0800

    Commit 4-print_alphabt.c file

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,13 +13,13 @@ int main(void)
 	char E;
 	char Q;
 
-	E = 'q';
-	Q = 'e';
+	E = 'e';
+	Q = 'q';
 
 	for (smol = 'a'; smol <= 'z'; smol++)
 	{
 		if (smol != E && smol != Q)
-			putchar(small);
+			putchar(smol);
 	}
 	putchar('\n');
 	return (0);
