79fe034567c8cce687323bbbe61b2d7bb8cf1e87
Remilekun <remmylknolaotan@gmail.com>
2022-12-13 13:04:02 -0800

    Add 0-putchar.c, main.h, _putchar.c file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - prints input using standard i/o library
+ * @c: the character to print
+ *
+ * print_alphabet - print all lowercase alphabets
+ *
+ * print_alphabet_x10 - print all alphabets in lowercase ten times
+ *
+ * File: main.h
+ * Author: Remilekun Olayinka
+ * A header file containting function prototypes
+ */
+
+int _putchar(char c);
+#endif
