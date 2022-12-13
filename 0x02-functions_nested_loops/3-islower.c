79be556abda874b17e3f3ac28d12100dee4904a0
Remilekun <remmylknolaotan@gmail.com>
2022-12-13 14:58:23 -0800

    Modify 3-islower.c file

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,14 +2,14 @@
 
 /**
  * _islower - Entry point
+ * @input: character to check it's case
  *
  * Dscription: Check if input is lower case
- * Return: 1 if input is lowercase
- * Return: 0 if input is not lowercase
+ * Return: 1 if input is lowercase, and 0 if input is not lowercase
  */
 int _islower(int input)
 {
-	if (input >=97 && input <= 122)
+	if (input >= 97 && input <= 122)
 	{
 		return (1);
 	}
