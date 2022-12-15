e713df7a6c15acae1be6f817344aeafcdbe290ce
Remilekun <remmylknolaotan@gmail.com>
2022-12-15 12:37:46 -0800

    Add 1-isdigit.c file

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,14 @@
 #include "main.h"
+
+/**
+ * _isupper - checks the the input if its upper or not.
+ * @c: input from user
+ * Return: Always 1 or 0.
+ */
+int _isdigit(int c)
+{
+	if ((c >= 48) && (c <= 57))
+		return (1);
+
+	return (0);
+}
