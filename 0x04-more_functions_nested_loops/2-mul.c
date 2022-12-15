070a07619be08e841382021d51eb3bab65757059
Remilekun <remmylknolaotan@gmail.com>
2022-12-15 13:33:46 -0800

    Update 2-mul.c and 3-print_numbers.c files

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,8 +3,8 @@
 /**
  * mul - Multiplies two integer
  * @a: first value to be multiplied
- * #b: secons value to be multipied
- * 
+ * @b: secons value to be multipied
+ *
  * Return: Always 0 (Success)
  */
 int mul(int a, int b)
