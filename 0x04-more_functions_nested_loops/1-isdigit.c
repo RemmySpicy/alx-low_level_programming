f9a13e189b578df2b91424f6a17b0ad5bb118535
Remilekun <remmylknolaotan@gmail.com>
2022-12-15 12:39:43 -0800

    Modify 1-isdigit file

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper - checks the the input if its upper or not.
+ * _isdigit - checks the the input if its upper or not.
  * @c: input from user
  * Return: Always 1 or 0.
  */
