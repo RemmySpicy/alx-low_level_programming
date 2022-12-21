8bdcbfe38ebfd051142955c0a1bbbc04bce3f4ac
Remilekun <remmylknolaotan@gmail.com>
2022-12-20 17:26:32 -0800

    Add 3-puts.c file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to standard output
+ * @str: value to print to standard io
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+		_putchar(str[i]);
+}
